6913c7faf5d694ff32acdf8abd18746faa82278c
Tanvan <tanvan.13022014@gmail.com>
2025-10-25 22:17:07 +0700

    Create CommentResponse includes id, content, createdAt and UserSumary

---
+++ src/main/java/com/tanvan/blogapplication/dto/CommentResponse.java
@@ -0,0 +1,18 @@
+package com.tanvan.blogapplication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentResponse {
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+
+    private UserSummaryDTO author;
+}
